9cf982102f3d95385060b03143fe7639ad2d217b
SPPL23 <seanpatricklacsina@gmail.com>
2025-12-06 16:11:21 +0800

    Deploy with correct base href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2810091278"
+    serviceWorkerVersion: "3252404287"
   }
 });
